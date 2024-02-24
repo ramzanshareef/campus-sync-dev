66f26d723ef0b33d15de47564c23b4918db39924
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-02-24 07:09:55 +0530

    made user signup, login files

---
+++ ui/user/LoginForm.js
@@ -0,0 +1,9 @@
+"use client";
+
+const LoginForm = () => {
+    return (
+        <div>LoginForm</div>
+    );
+};
+
+export default LoginForm;
---
+++ ui/user/SignUpForm.js
@@ -0,0 +1,9 @@
+"use client";
+
+const SignUpForm = () => {
+    return (
+        <div>SignUpForm</div>
+    );
+};
+
+export default SignUpForm;
